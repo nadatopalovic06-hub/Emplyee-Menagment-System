70948eaad97c0d5ca4eb47737e7b82d476f206dd
Nada Topalović <nada.topalovic06@gmail.com>
2026-03-05 23:13:23 +0100

    Update Radnici.py

--- Radnici.py
+++ Radnici.py
@@ -8,106 +8,118 @@ Created on Sun Dec  1 23:44:43 2024
 from datetime import date, datetime
 
 
-#učitavanje radnika iz fajla
-def loadWorkers():
+# loading employees from file
+def loadEmployees():
     for line in open('radnici.txt', 'r').readlines():
         if len(line) > 1:
-            rad = str2Radnik(line)
-            radnici.append(rad)
+            emp = str2Employee(line)
+            employees.append(emp)
 
 
-#upisuje novog radnika u fajl
-def saveRadniks():
-    fajl = open('radnici.txt', 'w')
-    for rad in radnici:
-        fajl.write(radnik2str(rad))
-        fajl.write('\n')
-    fajl.close()
+# writes a new employee to the file
+def saveEmployees():
+    file = open('radnici.txt', 'w')
+    for emp in employees:
+        file.write(employee2str(emp))
+        file.write('\n')
+    file.close()
     
 
-#trazi radnika po id broju
-def findRadnik(indeks): 
-    for rad in radnici:
-        if rad['id'] == indeks:
-            return rad
+# finds an employee by id number
+def findEmployee(index): 
+    for emp in employees:
+        if emp['id'] == index:
+            return emp
     return None
     
 
-#traži radnike po prezimenu  #field ce bitit prezime da je po mestu onda bi fail bilo mesto  
-#pošto više radnika može imati isto prezime, vrća listu radnika   
-def searchRadniks(field, value): 
+# searches employees by last name
+# field can be last name (if searching by city then field would be city)
+# since multiple employees can have the same last name, it returns a list
+def searchEmployees(field, value): 
     result = []
-    for rad in radnici:
-        if rad[field].upper() == value.upper():
-            result.append(rad)
+    for emp in employees:
+        if emp[field].upper() == value.upper():
+            result.append(emp)
     return result
 
-#dodaje novog radnika u listu
-def addRadnik(rad): 
-    radnici.append(rad)
+
+# adds a new employee to the list
+def addEmployee(emp): 
+    employees.append(emp)
     
-#menja podatke konkretnog radnika (ovde menja radno mesto)
-def updateRadnik(index, rad): 
-    radnici[index] = rad
+
+# updates data of a specific employee (here it changes the job position)
+def updateEmployee(index, emp): 
+    employees[index] = emp
 
 
-#cita liniju iz fajla i 'pretvara' je u Radnika (kljucevima u recniku dodeljuje vrednosti)
-def str2Radnik(line):
-    id, ime, prezime, datumRodjenja, datumRodjenja, email, plata, radnoMesto = line.strip().split("|")
-    rad = {'id': id,
-            'ime': ime,
-            'prezime': prezime,
-            'datumRodjenja': datumRodjenja,
-            'datumZaposlenja': datumRodjenja,
+# reads a line from the file and converts it into an Employee
+# (assigns values to dictionary keys)
+def str2Employee(line):
+    id, firstName, lastName, birthDate, birthDate, email, salary, position = line.strip().split("|")
+    emp = {'id': id,
+            'firstName': firstName,
+            'lastName': lastName,
+            'birthDate': birthDate,
+            'employmentDate': birthDate,
             'email': email,
-            'plata': plata,
-            'radnoMesto': radnoMesto}
-    return rad
+            'salary': salary,
+            'position': position}
+    return emp
 
-#priprema string za upisivanje u fajl
-def radnik2str(rad):    
-    return '|'.join([rad['id'], rad['ime'], rad['prezime'], 
-                     rad['datumRodjenja'], rad['datumZaposlenja'], 
-                     rad['email'], str(rad['plata']), rad['radnoMesto']])
+
+# prepares string for writing to file
+def employee2str(emp):    
+    return '|'.join([emp['id'], emp['firstName'], emp['lastName'], 
+                     emp['birthDate'], emp['employmentDate'], 
+                     emp['email'], str(emp['salary']), emp['position']])
      
-#traži sledeći slobodan id broj
-def maxId():
-    return len(radnici) + 1
 
-#header za ispisivanje podataka o radnicima
+# finds the next available id number
+def maxId():
+    return len(employees) + 1
+
+
+# header for displaying employee data
 def formatHeader():
     return \
-      "Id  |Ime     |Prezime     |Datum rodj.|Datum rodj.|Email           |Plata        |Radno mesto\n" \
+      "Id  |Name    |Last name   |Birth date |Birth date |Email           |Salary       |Position\n" \
       "---+--------+------------+-----------+-----------+----------------+-------------+-----------"
 
-def formatRadnik(rad):
+
+def formatEmployee(emp):
     return u"{0:3}|{1:<7}|{2:10}|{3:10}|{4:10}|{5:20}|{6:>6}|{7:>10}".format(
-      rad['id'],
-      rad['ime'],
-      rad['prezime'],
-      rad['datumRodjenja'],
-      rad['datumZaposlenja'],
-      rad['email'],
-      rad['plata'],
-      rad['radnoMesto'])
-
-#za svakog Radnika ispisuje po jedan red ispod headera
-def formatRadniks(radList): #treba da moze da formatira bilo oju listu oju joj prosledim
-    return "\n".join(map(formatRadnik, radList))
-
-#ovo ispisivanje može i jednostavnije, ostalo mi iz prethodnog projekta
-def formatAllRadniks():
-    return formatRadniks(radnici)
+      emp['id'],
+      emp['firstName'],
+      emp['lastName'],
+      emp['birthDate'],
+      emp['employmentDate'],
+      emp['email'],
+      emp['salary'],
+      emp['position'])
 
 
-def sviRadnici():
-    return radnici    
+# prints one row for each employee below the header
+def formatEmployees(empList): # should be able to format any list passed to it
+    return "\n".join(map(formatEmployee, empList))
+
+
+# this printing could be simpler, left from a previous project
+def formatAllEmployees():
+    return formatEmployees(employees)
+
+
+def allEmployees():
+    return employees    
 
     
-def sortirajRadnike(key):
-    return sorted(radnici, key = lambda x: x[key])
+def sortEmployees(key):
+    return sorted(employees, key = lambda x: x[key])
     
+
 print(__name__)  
-radnici = []
-loadRadniks() 
+employees = []
+loadEmployees()
+
 
